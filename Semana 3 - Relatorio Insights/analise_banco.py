6c7ddbdd70c49e31025aae1354999c614ce583e4
Moreira-Ruan <ruanmoreira.dev@gmail.com>
2025-09-04 17:27:00 -0300

    Update

--- Semana 3 - Relatorio Insights/analise_banco.py
+++ Semana 3 - Relatorio Insights/analise_banco.py
@@ -9,7 +9,7 @@ df = pd.read_excel('bank_marketing.xlsx', header=1)
 # Remove os espaços em branco extras dos nomes das colunas
 df.columns = df.columns.str.strip()
 
-# Agora o nome da coluna deve estar correto
+
 comprou = df[df['Cliente_Comprou_o_Titulo?'] == 'Sim']
 nao_comprou = df[df['Cliente_Comprou_o_Titulo?'] == 'Não']
 
@@ -23,13 +23,11 @@ plt.ylabel('Número de Clientes')
 plt.legend()
 plt.savefig('distribuicao_idade.png')
 
-# Seu código para carregar os dados aqui
-
 # Crie uma tabela de contingência para ver a relação entre Profissão e Compra
 contingencia = pd.crosstab(df['Profissão'], df['Cliente_Comprou_o_Titulo?'], normalize='index') * 100
 print(contingencia)
 
-# Opcional: crie um gráfico para visualizar
+# crie um gráfico para visualizar
 contingencia.plot(kind='bar', figsize=(12, 7))
 plt.title('Taxa de Compra por Profissão')
 plt.ylabel('Porcentagem de Compra')
@@ -44,7 +42,7 @@ plt.savefig('compra_por_profissao.png') # Salva o gráfico
 contingencia_devedor = pd.crosstab(df['Cliente_Devedor?'], df['Cliente_Comprou_o_Titulo?'], normalize='index') * 100
 print(contingencia_devedor)
 
-# Opcional: crie um gráfico para visualizar
+# crie um gráfico para visualizar
 contingencia_devedor.plot(kind='bar', figsize=(8, 6))
 plt.title('Taxa de Compra por Histórico de Inadimplência')
 plt.ylabel('Porcentagem de Compra')
@@ -58,7 +56,7 @@ plt.savefig('compra_por_devedor.png')
 contingencia_hipoteca = pd.crosstab(df['Tem_Hipoteca?'], df['Cliente_Comprou_o_Titulo?'], normalize='index') * 100
 print(contingencia_hipoteca)
 
-# Opcional: crie um gráfico para visualizar
+# crie um gráfico para visualizar
 contingencia_hipoteca.plot(kind='bar', figsize=(8, 6))
 plt.title('Taxa de Compra por Posse de Hipoteca')
 plt.ylabel('Porcentagem de Compra')
@@ -72,7 +70,7 @@ plt.savefig('compra_por_hipoteca.png')
 contingencia_emprestimo = pd.crosstab(df['Tem_Emprestimo?'], df['Cliente_Comprou_o_Titulo?'], normalize='index') * 100
 print(contingencia_emprestimo)
 
-# Opcional: crie um gráfico para visualizar
+# crie um gráfico para visualizar
 contingencia_emprestimo.plot(kind='bar', figsize=(8, 6))
 plt.title('Taxa de Compra por Posse de Empréstimo')
 plt.ylabel('Porcentagem de Compra')
